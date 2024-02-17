bbe677f0426f183399fb89506a8436d89b4b665b
Shivtej Ghatage <shivtejmailbox@gmail.com>
2024-02-17 11:59:18 +0530

    Create Profile API endpoint using Django

---
+++ backend/profiles/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+from .models import Profile
+from .serializers import ProfileSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+
+# Create your views here.
+@api_view(['GET', 'POST'])
+def profile_list(request):
+    if request.method == 'GET':
+        profiles = Profile.objects.all()
+        serializer = ProfileSerializer(profiles, many = True)
+        return JsonResponse({'profiles': serializer.data})
+    
+    if request.method == 'POST':
+        serializer = ProfileSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
