8880beca373c9f529fb8e8560b611426c69f1080
Geet Sethi <geetsethi05@gmail.com>
2024-03-19 14:15:00 +0530

    use built-in views for login and token refresh

--- backend/account/urls.py
+++ backend/account/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
-from account.views import register, login
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+from .views import register
 
 urlpatterns = [
-    path('register/', register, name='register'),
-    path('login/', login, name='login'),
+    path("register/", register, name="register"),
+    # path('login/', login, name='login'),
+    path("login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("login/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
