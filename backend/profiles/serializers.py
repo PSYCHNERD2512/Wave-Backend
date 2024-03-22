a335ed53b0f6d96db27726a270c94747a8a40558
tanishqmm <tanishqmandhane@gmail.com>
2024-03-22 16:42:15 +0530

    error fix

--- backend/profiles/serializers.py
+++ backend/profiles/serializers.py
@@ -5,7 +5,7 @@ from django.contrib.auth import get_user_model
 Profile = get_user_model()
 
 class ProfileSerializer(serializers.ModelSerializer):
-    connections = serializers.PrimaryKeyRelatedField(many=True)
+    connections = serializers.PrimaryKeyRelatedField(many=True,queryset=Profile.objects.all())
     sent_waves = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
     received_waves = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
     class Meta:
