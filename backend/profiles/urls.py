f9f679f5c59a082fe5fc4cca887cd78fe661b327
Achintya <aar012905@gmail.com>
2024-03-23 11:30:42 +0530

    deleted redundant urls

--- backend/profiles/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('send_wave/<int:sender_id>/<int:receiver_id>/', views.send_wave, name='send_wave'),
-    path('accept_wave/<int:request_id>/', views.accept_wave, name='accept_wave'),
-]
